5f41774a0ae49d527d746032fdf900f57767d46c
PrakashPatel <patelprakash21@gmail.com>
2023-10-10 16:35:17 +0530

    Update StudentRoutes.js

--- backend/src/routes/StudentRoutes.js
+++ backend/src/routes/StudentRoutes.js
@@ -11,6 +11,7 @@ const { UserToken, restrict, StudentToken } = require("../middleware/Auth");
 router.post("/studentregister", UserToken,restrict("admin"),StudentData.StudentRegister);
 router.post("/studentlogin", StudentData.studentLogin);
 router.get("/studentview", StudentToken,StudentData.studentFind);
+router.get("/studentfindall", UserToken,restrict("admin"),StudentData.studentFindAll);
 router.post("/studentlogout", StudentToken,StudentData.studentLogout);
 router.delete("/studentdelete", StudentToken,StudentData.studentDelete);
 
